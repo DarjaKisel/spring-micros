875ac37799f51f9832f6d56f42e2dcf6d13443cb
dzinevich <darjakisel@gmail.com>
2021-12-05 23:41:28 +0200

    Add build services script

--- build-services.sh
+++ build-services.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-for serviceDirectory in gateway
+for serviceDirectory in discovery config service-producer service-consumer gateway
 do
  pushd ./$serviceDirectory
  ./gradlew clean check bootJar
