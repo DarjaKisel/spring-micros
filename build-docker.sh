ee05512524313b734b01a26744fd86606e16200d
dzinevich <darjakisel@gmail.com>
2021-12-06 23:25:52 +0200

    Added run-docker script and docker-compose

---
+++ build-docker.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -xe
+
+for serviceName in discovery config service-producer service-consumer gateway
+do
+ pushd ./$serviceName
+ docker build --build-arg JAR_FILE=build/libs/\*.jar -t dzinevich/$serviceName -f ./Dockerfile .
+ popd
+done
---
+++ run-docker.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -xe
+
+#./build.sh
+#./build-docker.sh
+
+
+docker-compose up -d
+
